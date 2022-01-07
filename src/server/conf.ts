d97a9a35e71ec45aa8a6e42fe1fc4054d14f5030
XpniagaDevs <dnang99@gmail.com>
2022-01-08 06:16:10 +0700

    Update conf.ts

--- src/server/conf.ts
+++ src/server/conf.ts
@@ -4,7 +4,7 @@ export const Configuration: IConfiguration = {
     WBNB_CONTRACT: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
     ROUTER: "0x10ED43C718714eb63d5aA57B78B54704E256024E", // Pancake V2 Router
     FACTORY: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73", // Pancake V2 Factory
-    WALLET_PUBLIC: "0x000000....", // Your public wallet address
+    WALLET_PUBLIC: "0x", // Your public wallet address
     WALLET_PRIVATE: "0x000000....", // Your private wallet address
     SLIPPAGE: 10, // Default slippage
     GWEI: 5,
